d5dbae3fe3476f1bbb5dee801bfd1aa49cfba163
Konrads <g.a.donayre.business@gmail.com>
2025-02-13 09:22:01 +0800

    We added a configuration in the next.config.ts to allow external images from googleusercontent. Front end logic of the profile page, it's done na pero the backend stuff and how will we get the data is ano pa ewan. Ayun lang.

--- src/app/(student)/dashboard/page.tsx
+++ src/app/(student)/dashboard/page.tsx
@@ -1,14 +1,17 @@
 import DashboardTable from "@/components/DashboardTable";
 import React from "react";
 import { consultations } from "@/data";
+import { auth } from "@/app/auth";
 
-const page = () => {
-  const name = "George";
+const page = async () => {
+  const session = await auth();
+  const user = session?.user;
+  const firstName = user?.name?.split(" ")[0] || "Guest";
 
   return (
     <section className="px-6 py-2 md:px-12 md:py-3">
       <div className="mb-6 flex flex-col gap-3">
-        <p className="text-3xl font-bold md:text-5xl">Hello, {name}🫡</p>
+        <p className="text-3xl font-bold md:text-5xl">Hello, {firstName}🫡</p>
         <p className="text-lg font-semibold md:text-xl">
           Here&apos;s an overview of your consultations.
         </p>
--- src/app/(student)/profile/page.tsx
+++ src/app/(student)/profile/page.tsx
@@ -1,7 +1,68 @@
+import Image from "next/image";
 import React from "react";
+import { auth } from "@/app/auth";
 
-const page = () => {
-  return <div>Profile</div>;
+const page = async () => {
+  const session = await auth();
+  const user = session?.user;
+  const studentMockData = {
+    studentId: "202403214",
+    degreeProgram: "Bs Computer Science",
+    zodiacSign: "Pisces",
+    saisId: "12345678",
+  };
+  return (
+    <section className="px-6 py-1 uppercase md:px-24 md:py-2">
+      <div className="mb-6 flex items-center gap-4">
+        <Image
+          className="rounded-full"
+          src={user?.image || "/profile-user-icon.svg"}
+          alt="user profile picture"
+          height={180}
+          width={180}
+        />
+        <p className="text-2xl font-bold md:text-4xl">{user?.name}</p>
+      </div>
+      <div className="px-2 sm:px-4">
+        <h2 className="text-2x mb-3 font-bold uppercase">
+          General Information
+        </h2>
+        <div className="flex w-full flex-col justify-between sm:flex-row lg:w-5/6">
+          {/* TODO: bday to zodiac sign and the db for this shit. */}
+          <div>
+            <ul className="space-y-2">
+              <li>
+                <span className="font-semibold">Name</span>: {user?.name}
+              </li>
+              <li>
+                <span className="font-semibold">Email</span>: {user?.email}
+              </li>
+              <li>
+                <span className="font-semibold">Studen Id</span>:{" "}
+                {studentMockData.studentId}
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul className="space-y-2">
+              <li>
+                <span className="font-semibold">Degree Program</span>:{" "}
+                {studentMockData.degreeProgram}
+              </li>
+              <li>
+                <span className="font-semibold">Zodiac Sign</span>:{" "}
+                {studentMockData.zodiacSign}
+              </li>
+              <li>
+                <span className="font-semibold">Sais Id</span>:{" "}
+                {studentMockData.saisId}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default page;
